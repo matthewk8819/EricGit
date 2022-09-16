c232d4b55947b1b5a781355a941d29cbe6cc69f9
wyatt <wanand1@hwemail.com>
2022-09-15 17:09:23 -0700

    Wrote method to create new file titled with sha1
    Should include the elements from arraylist

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -8,7 +11,7 @@ public class Tree {
 	ArrayList<String> pairs = new ArrayList<String>();
 	String combinedString;
 	String sha1;
-	public Tree(ArrayList p) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+	public Tree(ArrayList<String> p) throws UnsupportedEncodingException, NoSuchAlgorithmException {
 		pairs = p;
 		for (int i = 0; i < pairs.size(); i++) {
 			combinedString += pairs.get(i);
@@ -17,5 +20,24 @@ public class Tree {
 		digest.reset(); 
 		digest.update(combinedString.getBytes("utf8"));
 		sha1 = String.format("%040x", new BigInteger(1, digest.digest()));
+	}
+	public void storeArrayList() throws IOException {
+		File treeFile = new File("/objects/" + sha1);
+		FileWriter myWriter = new FileWriter(treeFile);
+		for (int i = 0; i < pairs.size(); i++) {
+			myWriter.write(pairs.get(i));
+		}
+		myWriter.close();
+	}
+	
+	public static void main(String [] args) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("blob : 81e0268c84067377a0a1fdfb5cc996c93f6dcf9f");
+		list.add("blob : 01d82591292494afd1602d175e165f94992f6f5f");
+		list.add("blob : f1d82236ab908c86ed095023b1d2e6ddf78a6d83");
+		list.add("tree : bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b");
+		list.add("tree : e7d79898d3342fd15daf6ec36f4cb095b52fd976");
+		Tree t = new Tree(list);
+		t.storeArrayList();
 	}
 }
